1cfaa9ff9d6179861afad2976fb75a0fbc0e7fa9
xsj520 <72717602+xsj520@users.noreply.github.com>
2026-04-10 19:23:52 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Openwrt'/g" package/base-files/files/bin/config_generate
 # 修改版本为编译日期
-date_version=$(date +"26.3.30")
+date_version=$(date -d "2026-03-30" +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by xsj/g" package/lean/default-settings/files/zzz-default-settings
 
