6615e7f1f5a76de6e1137e660bd80cdb1da80979
xsj520 <72717602+xsj520@users.noreply.github.com>
2026-05-09 20:30:02 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -102,6 +102,7 @@ sed -i 's/"SQM 队列管理"/"网络控制"/g' `grep "SQM 队列管理" -rl ./`
 sed -i 's/"进程"/"进程情况"/g' `grep "进程" -rl ./`
 sed -i 's/"ShadowSocksR Plus+"/"科学上网"/g' `grep "ShadowSocksR Plus+" -rl ./`
 sed -i 's/"Plugins"/"全局设置"/g' `grep "Plugins" -rl ./`
+sed -i 's/"插件"/"插件设置"/g' `grep "插件" -rl ./`
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
